57cb65a2e6357e6822ce5a43265b9e78c5840249
Tu Anh Huynh <tuanhlyhuynh@gmail.com>
2020-10-28 09:59:18 -0400

    [JS] Added JS to add another item in drop down.

---
+++ app.js
@@ -0,0 +1,12 @@
+{/* <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+<a class="dropdown-item" href="#">Nike</a>
+<a class="dropdown-item" href="#">Jordan</a>
+<a class="dropdown-item" href="#">Adidas</a>
+<a class="dropdown-item" href="#">Yeezy</a>
+<a class="dropdown-item" href="#">Asics</a>
+<a class="dropdown-item" href="#">Puma</a> */}
+
+const item = document.createElement('a');
+item.classList.add('dropdown-item');
+item.setAttribute('href','#');
+item.textContent = 'Collaborations';
